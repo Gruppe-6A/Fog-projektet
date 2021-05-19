840ef1090bbee8fdebc96c5b80b7a0f3b3d73fc1
Thomas Amorsen <cph-ta181@cphbusiness.dk>
2021-05-19 13:18:47 +0200

    Picasso

--- src/main/java/business/services/SVG.java
+++ src/main/java/business/services/SVG.java
@@ -17,6 +17,13 @@ public class SVG {
             "preserveAspectRatio=\"xMinYMin\">";
 
     private final String rectTemplate = "<rect x=\"%f\" y=\"%f\" height=\"%f\" width=\"%f\" style=\"stroke:#000000; fill: #ffffff\" />";
+    private final String lineTemplate = "<g stroke=\"black\" stroke-width=\"4\"><path stroke-dasharray=\"5,5\" d=\"M%d %d l%d %d\" />";
+    private final String defs = "<defs> <marker id=\"beginArrow\" markerWidth=\"12\" markerHeight=\"12\" refX=\"0\" refY=\"6\" orient=\"auto\"> <path d=\"M0,6 L12,0 L12,12 L0,6\" style=\"fill: #000000;\" /> </marker> <marker id=\"endArrow\" markerWidth=\"12\" markerHeight=\"12\" refX=\"12\" refY=\"6\" orient=\"auto\"> <path d=\"M0,0 L12,6 L0,12 L0,0 \" style=\"fill: #000000;\" /> </marker> </defs>";
+    private final String arrowLine = "<line x1=\"%f\"  y1=\"%f\" x2=\"%f\"   y2=\"%f\" \n" +
+            "\tstyle=\"stroke: #000000;\n" +
+            "\tmarker-start: url(#beginArrow);\n" +
+            "\tmarker-end: url(#endArrow);\"/>";
+    private final String textTemplate = "<text style=\"text-anchor: middle\" transform=\"translate(%f,%f) rotate(%f)\">%s</text>";
 
     public SVG(int x, int y, String viewBox, int width, int height)
     {
@@ -28,6 +35,18 @@ public class SVG {
         svg.append(String.format(headerTemplate, height, width, viewBox, x, y ));
     }
 
+    public void addDefs(){
+        svg.append(defs);
+    }
+
+    public  void addText(double x, double y, double rotation, String text){
+        svg.append(String.format(textTemplate, x, y, rotation, text));
+    }
+
+    public void addArrowLine(double x1, double y1, double x2, double y2){
+        svg.append(String.format(arrowLine, x1, y1, x2, y2));
+    }
+
     public void addRect(double x, double y, double height, double width)
     {
         svg.append(String.format(rectTemplate, x, y, height, width));
@@ -35,7 +54,7 @@ public class SVG {
 
     public void addLine(int x1, int y1, int x2, int y2 )
     {
-
+        svg.append(String.format(lineTemplate, x1, y1, x2, y2));
     }
 
     public void addSvg(SVG innerSVG)
--- src/main/java/web/commands/ShowSVGCommand.java
+++ src/main/java/web/commands/ShowSVGCommand.java
@@ -27,17 +27,44 @@ public class ShowSVGCommand extends CommandUnprotectedPage{
         Order order = orderFacade.getOrder(orderId);
         int length = order.getLength();
         int width = order.getWidth();
-        SVG svg = new SVG(0, 0, "0 0 800 600", length, width);
+        SVG svg = new SVG(100, 0, "0 0 " + length + " " + width, length, width);
         svg.addRect(0, 35.0, 4.5, length);
         svg.addRect(0, width-39.5, 4.5, length);
-        /*
-        for (int x = 0; x < 14; x++)
+
+        for (int x = 0; x < length/55 ; x++)
         {
-            svg.addRect(100 + 50 * x, 0, 600.0, 4.5);
+            svg.addRect(0 + 55 * x, 0, length, 4.5);
+        }
+        svg.addRect(length-4.5, 0, width, 4.5);
+
+        for (int x = 0; x < Math.ceil((float)length/310); x++){
+            svg.addRect(length-10 - 310 * x, 32.4, 9.7, 9.7);
+        }
+        for (int x = 0; x < Math.ceil((float)length/310); x++){
+            svg.addRect(length-10 - 310 * x, width-42.1, 9.7, 9.7);
         }
 
-         */
-        request.setAttribute("svgdrawing", svg.toString());
+        int xOffset = length/4;
+        svg.addLine(xOffset,35 ,xOffset*2,width-70);
+        svg.addLine(length-xOffset,35 ,-xOffset*2,width-70);
+
+        SVG svgMeasurements = new SVG(0, 0, "0 0 " + (length+100) + " " + (width+100), length+100, width+100);
+        svgMeasurements.addDefs();
+        svgMeasurements.addRect(0,0,width+100, length+100);
+        svgMeasurements.addRect(100,width,1,length);
+        svgMeasurements.addArrowLine(35,0, 35, width);
+        svgMeasurements.addText(25, width/2, -90, width+" cm.");
+
+        svgMeasurements.addArrowLine(85,35, 85, width-35);
+        svgMeasurements.addText(75, width/2, -90, (width-70)+" cm.");
+
+        svgMeasurements.addArrowLine(100,width+45, length+100, width+45);
+        svgMeasurements.addText(length/2+100, width+60, 0, length+" cm.");
+
+        svgMeasurements.addSvg(svg);
+
+        request.setAttribute("svgdrawing", svgMeasurements.toString());
+        //request.setAttribute("svgdrawing", svg.toString());
         return pageToShow;
     }
 }
