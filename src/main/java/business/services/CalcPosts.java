1029c0e1339f21433054e855ef2fcc7f478eaa7f
Thomas Amorsen <cph-ta181@cphbusiness.dk>
2021-05-25 14:43:07 +0200

    fixes minimum post count in CalcPosts

--- src/main/java/business/services/CalcPosts.java
+++ src/main/java/business/services/CalcPosts.java
@@ -14,7 +14,14 @@ public class CalcPosts {
 
     public Result calculate(){
         int post_length = 300;
-        int post_count = 4 + (int)Math.ceil((float)(length-640)/310)*2;
+        int post_count;
+        if (length<310){
+            post_count = 4;
+        }
+        else{
+            post_count = 2 + (int)Math.ceil((float)(length-310)/310)*2;
+        }
+
 
         return new Result(post_length, post_count, materials_id);
     }
